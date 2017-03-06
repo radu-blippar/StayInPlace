2a2add7527150ca89f91ad41fe3df51ec8c31660
radu_nicolau <radu.nicolau@blippar.com>
2017-03-06 10:45:48 +0000

    look at solved without parent

--- lookat test/main.js
+++ lookat test/main.js
@@ -14,26 +14,217 @@ function rr(min, max) { //RandomRange
   return (min + Math.random() * (max - min))
 }
 
+function toDegrees(angle) {
+  return angle * (180 / Math.PI);
+}
+
+function toRadians(angle) {
+  return angle * (Math.PI / 180);
+}
+
+function Sin(deg) {
+  return Math.sin(toRadians(deg))
+}
+
+function Cos(deg) {
+  return Math.cos(toRadians(deg))
+}
+
+function multiplyMatrix(b, a) {
+  var aNumRows = a.length,
+    aNumCols = a[0].length,
+    bNumRows = b.length,
+    bNumCols = b[0].length,
+    m = new Array(aNumRows); // initialize array of rows
+  for (var r = 0; r < aNumRows; ++r) {
+    m[r] = new Array(bNumCols); // initialize the current row
+    for (var c = 0; c < bNumCols; ++c) {
+      m[r][c] = 0; // initialize the current cell
+      for (var i = 0; i < aNumCols; ++i) {
+        m[r][c] += a[r][i] * b[i][c];
+      }
+    }
+  }
+  return m;
+}
+
+function matrixInvert(m) {
+  var det = matrixDeterminant(m);
+  var a = undefined;
+  if (det != 0) {
+    a = matrixAdj(m);
+    for (var i = 0; i < 4; i++) {
+      for (var j = 0; j < 4; j++) {
+        a[i][j] /= det;
+      }
+    }
+  }
+  return a
+}
+
+function matrixAdj(m) {
+  var a = [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
+  ];
+
+  a[0][0] = m[1][2] * m[2][3] * m[3][1] - m[1][3] * m[2][2] * m[3][1] + m[1][3] * m[2][1] * m[3][2] - m[1][1] * m[2][3] * m[3][2] - m[1][2] * m[2][1] * m[3][3] + m[1][1] * m[2][2] * m[3][3];
+  a[0][1] = m[0][3] * m[2][2] * m[3][1] - m[0][2] * m[2][3] * m[3][1] - m[0][3] * m[2][1] * m[3][2] + m[0][1] * m[2][3] * m[3][2] + m[0][2] * m[2][1] * m[3][3] - m[0][1] * m[2][2] * m[3][3];
+  a[0][2] = m[0][2] * m[1][3] * m[3][1] - m[0][3] * m[1][2] * m[3][1] + m[0][3] * m[1][1] * m[3][2] - m[0][1] * m[1][3] * m[3][2] - m[0][2] * m[1][1] * m[3][3] + m[0][1] * m[1][2] * m[3][3];
+  a[0][3] = m[0][3] * m[1][2] * m[2][1] - m[0][2] * m[1][3] * m[2][1] - m[0][3] * m[1][1] * m[2][2] + m[0][1] * m[1][3] * m[2][2] + m[0][2] * m[1][1] * m[2][3] - m[0][1] * m[1][2] * m[2][3];
+  a[1][0] = m[1][3] * m[2][2] * m[3][0] - m[1][2] * m[2][3] * m[3][0] - m[1][3] * m[2][0] * m[3][2] + m[1][0] * m[2][3] * m[3][2] + m[1][2] * m[2][0] * m[3][3] - m[1][0] * m[2][2] * m[3][3];
+  a[1][1] = m[0][2] * m[2][3] * m[3][0] - m[0][3] * m[2][2] * m[3][0] + m[0][3] * m[2][0] * m[3][2] - m[0][0] * m[2][3] * m[3][2] - m[0][2] * m[2][0] * m[3][3] + m[0][0] * m[2][2] * m[3][3];
+  a[1][2] = m[0][3] * m[1][2] * m[3][0] - m[0][2] * m[1][3] * m[3][0] - m[0][3] * m[1][0] * m[3][2] + m[0][0] * m[1][3] * m[3][2] + m[0][2] * m[1][0] * m[3][3] - m[0][0] * m[1][2] * m[3][3];
+  a[1][3] = m[0][2] * m[1][3] * m[2][0] - m[0][3] * m[1][2] * m[2][0] + m[0][3] * m[1][0] * m[2][2] - m[0][0] * m[1][3] * m[2][2] - m[0][2] * m[1][0] * m[2][3] + m[0][0] * m[1][2] * m[2][3];
+  a[2][0] = m[1][1] * m[2][3] * m[3][0] - m[1][3] * m[2][1] * m[3][0] + m[1][3] * m[2][0] * m[3][1] - m[1][0] * m[2][3] * m[3][1] - m[1][1] * m[2][0] * m[3][3] + m[1][0] * m[2][1] * m[3][3];
+  a[2][1] = m[0][3] * m[2][1] * m[3][0] - m[0][1] * m[2][3] * m[3][0] - m[0][3] * m[2][0] * m[3][1] + m[0][0] * m[2][3] * m[3][1] + m[0][1] * m[2][0] * m[3][3] - m[0][0] * m[2][1] * m[3][3];
+  a[2][2] = m[0][1] * m[1][3] * m[3][0] - m[0][3] * m[1][1] * m[3][0] + m[0][3] * m[1][0] * m[3][1] - m[0][0] * m[1][3] * m[3][1] - m[0][1] * m[1][0] * m[3][3] + m[0][0] * m[1][1] * m[3][3];
+  a[2][3] = m[0][3] * m[1][1] * m[2][0] - m[0][1] * m[1][3] * m[2][0] - m[0][3] * m[1][0] * m[2][1] + m[0][0] * m[1][3] * m[2][1] + m[0][1] * m[1][0] * m[2][3] - m[0][0] * m[1][1] * m[2][3];
+  a[3][0] = m[1][2] * m[2][1] * m[3][0] - m[1][1] * m[2][2] * m[3][0] - m[1][2] * m[2][0] * m[3][1] + m[1][0] * m[2][2] * m[3][1] + m[1][1] * m[2][0] * m[3][2] - m[1][0] * m[2][1] * m[3][2];
+  a[3][1] = m[0][1] * m[2][2] * m[3][0] - m[0][2] * m[2][1] * m[3][0] + m[0][2] * m[2][0] * m[3][1] - m[0][0] * m[2][2] * m[3][1] - m[0][1] * m[2][0] * m[3][2] + m[0][0] * m[2][1] * m[3][2];
+  a[3][2] = m[0][2] * m[1][1] * m[3][0] - m[0][1] * m[1][2] * m[3][0] - m[0][2] * m[1][0] * m[3][1] + m[0][0] * m[1][2] * m[3][1] + m[0][1] * m[1][0] * m[3][2] - m[0][0] * m[1][1] * m[3][2];
+  a[3][3] = m[0][1] * m[1][2] * m[2][0] - m[0][2] * m[1][1] * m[2][0] + m[0][2] * m[1][0] * m[2][1] - m[0][0] * m[1][2] * m[2][1] - m[0][1] * m[1][0] * m[2][2] + m[0][0] * m[1][1] * m[2][2];
+
+  return a;
+}
+
+function matrixDeterminant(m) {
+  var det =
+    m[0][3] * m[1][2] * m[2][1] * m[3][0] - m[0][2] * m[1][3] * m[2][1] * m[3][0] - m[0][3] * m[1][1] * m[2][2] * m[3][0] + m[0][1] * m[1][3] * m[2][2] * m[3][0] +
+    m[0][2] * m[1][1] * m[2][3] * m[3][0] - m[0][1] * m[1][2] * m[2][3] * m[3][0] - m[0][3] * m[1][2] * m[2][0] * m[3][1] + m[0][2] * m[1][3] * m[2][0] * m[3][1] +
+    m[0][3] * m[1][0] * m[2][2] * m[3][1] - m[0][0] * m[1][3] * m[2][2] * m[3][1] - m[0][2] * m[1][0] * m[2][3] * m[3][1] + m[0][0] * m[1][2] * m[2][3] * m[3][1] +
+    m[0][3] * m[1][1] * m[2][0] * m[3][2] - m[0][1] * m[1][3] * m[2][0] * m[3][2] - m[0][3] * m[1][0] * m[2][1] * m[3][2] + m[0][0] * m[1][3] * m[2][1] * m[3][2] +
+    m[0][1] * m[1][0] * m[2][3] * m[3][2] - m[0][0] * m[1][1] * m[2][3] * m[3][2] - m[0][2] * m[1][1] * m[2][0] * m[3][3] + m[0][1] * m[1][2] * m[2][0] * m[3][3] +
+    m[0][2] * m[1][0] * m[2][1] * m[3][3] - m[0][0] * m[1][2] * m[2][1] * m[3][3] - m[0][1] * m[1][0] * m[2][2] * m[3][3] + m[0][0] * m[1][1] * m[2][2] * m[3][3];
+  return det;
+}
+
+function printMatrix(a) {
+  var aNumRows = a.length;
+  var aNumCols = a[0].length;
+  console.log(' ┌');
+  var t = "";
+  for (var r = 0; r < aNumRows; ++r) {
+    t = " │";
+    for (var c = 0; c < aNumCols; ++c) {
+      if (Math.abs(a[r][c]) < 0.01) {
+        val = "0*";
+      } else {
+        val = "" + a[r][c];
+      }
+      t += " " + val.substr(0, 7);
+      if (c < aNumCols - 1) {
+        t += ", ";
+      }
+    }
+    console.log(t)
+  }
+  console.log(' └');
+}
+
+function XYZ_to_ZYX(rotations, inputRadians, returnRadians) {
+  var aa = rotations[0];
+  var bb = rotations[1];
+  var cc = rotations[2];
+
+  if (inputRadians) {
+    aa = toDegrees(aa);
+    bb = toDegrees(bb);
+    cc = toDegrees(cc);
+  }
+
+  var Rx = [
+    [1, 0, 0],
+    [0, Cos(aa), -Sin(aa)],
+    [0, Sin(aa), Cos(aa)]
+  ];
+
+  var Ry = [
+    [Cos(bb), 0, Sin(bb)],
+    [0, 1, 0],
+    [-Sin(bb), 0, Cos(bb)]
+  ];
+
+  var Rz = [
+    [Cos(cc), -Sin(cc), 0],
+    [Sin(cc), Cos(cc), 0],
+    [0, 0, 1]
+  ];
+
+  var IJK = [
+    [1, 0, 0],
+    [0, 1, 0],
+    [0, 0, 1]
+  ];
+
+  var IJKx = multiplyMatrix(IJK, Rx);
+  var IJKy = multiplyMatrix(IJKx, Ry);
+  var IJKz = multiplyMatrix(IJKy, Rz);
+
+  var out_x = Math.atan2(-IJKz[1][2], IJKz[2][2]);
+  var out_y = Math.atan2(IJKz[0][2], IJKz[2][2] * Math.cos(out_x) - IJKz[1][2] * Math.sin(out_x));
+  var out_z = Math.atan2(IJKz[1][0] * Math.cos(out_x) + IJKz[2][0] * Math.sin(out_x), IJKz[1][1] * Math.cos(out_x) + IJKz[2][1] * Math.sin(out_x));
+
+  if (returnRadians) {
+    return [out_x, out_y, out_z]
+  } else {
+    return [toDegrees(out_x), toDegrees(out_y), toDegrees(out_z)]
+  }
+}
+
+function Point(thePointer, theTarget) {
+
+  var tPointer = thePointer.getTranslation();
+  var tTarget = theTarget.getTranslation();
+
+  var d = [tTarget[0] - tPointer[0], tTarget[1] - tPointer[1], tTarget[2] - tPointer[2]];
+
+  var x = Math.atan2(-d[1], d[0]);
+  var y = Math.atan2(d[2], -Math.sqrt(d[0] * d[0] + d[1] * d[1]));
+
+  var A = [
+    [0, -Math.cos(y), Math.sin(y), 0],
+    [Math.cos(x), -Math.sin(x) * Math.sin(y), -Math.sin(x) * Math.cos(y), 0],
+    [Math.sin(x), Math.cos(x) * Math.sin(y), Math.cos(x) * Math.cos(y), 0],
+    [0, 0, 0, 1]
+  ];
+
+  var B = [
+    [0, 0, 1, tPointer[2]],
+    [0, 1, 0, tPointer[1]],
+    [1, 0, 0, tPointer[0]],
+    [0, 0, 0, 1]
+  ];
+
+  var m = multiplyMatrix(A, matrixInvert(B));
+
+  var t = [-m[0][3], -m[1][3], -m[2][3]];
+  var r = [];
+  r.push(Math.atan2(m[2][1], m[2][2]));
+  r.push(Math.atan2(-m[2][0], Math.sqrt(m[2][2] * m[2][2] + m[2][1] * m[2][1])));
+  r.push(Math.atan2(m[1][0], m[0][0]));
+
+  r = [toDegrees(r[0]), toDegrees(r[1]), toDegrees(r[2])];
+  r = XYZ_to_ZYX(r, false, false)
+
+  thePointer.setTranslation(t)
+  thePointer.setRotation(r);
+}
+
 scene.onCreate = function () {
 
-  p0 = scene.addTransform();
-
-  Pointer = p0.addMesh('Pointer.b3m').setColor('00ff00').setMaterial('Default');
+  Pointer = scene.addMesh('Pointer.b3m').setColor('00ff00').setMaterial('Default');
 
   Target = scene.addMesh('Target.b3m').setColor('ff0000aa').setMaterial('Default');
 
   Look = scene.getScreen().addSprite().setScale(sW / 2).setTranslation(sW / 2, -sH / 2.2, 0).setColor("00ff0040");
   Look.onTouchEnd = function () {
-    var p = [rr(-400, 400), rr(-400, 400), rr(-400, 400)];
-    var t = [rr(-400, 400), rr(-400, 400), rr(-400, 400)];
-    var d = [t[0] - p[0], t[1] - p[1], t[2] - p[2]];
-
-    var x = Math.atan2(-d[1], d[0]) * 180 / Math.PI;
-    var y = Math.atan2(-d[2], Math.sqrt(d[0] * d[0] + d[1] * d[1])) * 180 / Math.PI;
-
-    Target.setTranslation(t)
-    p0.setTranslation(p).setRotationY(90);
-    Pointer.setRotation(x, y, 90)
+    var new_p = [rr(-400, 400), rr(-400, 400), rr(-400, 400)];
+    var new_t = [rr(-400, 400), rr(-400, 400), rr(-400, 400)];
+    Target.setTranslation(new_t)
+    Point(Pointer, Target);
   }
 
   Look.onTouchEnd();
