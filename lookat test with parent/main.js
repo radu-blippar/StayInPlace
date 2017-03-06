15d7b9cf786e95224c8ed0b0fb909609a0d8dd17
radu_nicolau <radu.nicolau@blippar.com>
2017-03-06 10:49:04 +0000

    keep a copy

---
+++ lookat test with parent/main.js
@@ -0,0 +1,40 @@
+var blipp = require('blippar').blipp;
+var scene = blipp.addScene('default');
+scene.addMaterial('Default').setType('matt');
+
+var sW = blipp.getScreenWidth() * 1.003;
+var sH = blipp.getScreenHeight() * 1.003;
+
+Reset = scene.getScreen().addSprite().setScale(sW / 2).setTranslation(-sW / 2, -sH / 2.2, 0).setColor("ff000040");
+Reset.onTouchEnd = function () {
+  blipp.goToBlipp(blipp.getAddress());
+}
+
+function rr(min, max) { //RandomRange
+  return (min + Math.random() * (max - min))
+}
+
+scene.onCreate = function () {
+
+  p0 = scene.addTransform();
+
+  Pointer = p0.addMesh('Pointer.b3m').setColor('00ff00').setMaterial('Default');
+
+  Target = scene.addMesh('Target.b3m').setColor('ff0000aa').setMaterial('Default');
+
+  Look = scene.getScreen().addSprite().setScale(sW / 2).setTranslation(sW / 2, -sH / 2.2, 0).setColor("00ff0040");
+  Look.onTouchEnd = function () {
+    var p = [rr(-400, 400), rr(-400, 400), rr(-400, 400)];
+    var t = [rr(-400, 400), rr(-400, 400), rr(-400, 400)];
+    var d = [t[0] - p[0], t[1] - p[1], t[2] - p[2]];
+
+    var x = Math.atan2(-d[1], d[0]) * 180 / Math.PI;
+    var y = Math.atan2(-d[2], Math.sqrt(d[0] * d[0] + d[1] * d[1])) * 180 / Math.PI;
+
+    Target.setTranslation(t)
+    p0.setTranslation(p).setRotationY(90);
+    Pointer.setRotation(x, y, 90)
+  }
+
+  Look.onTouchEnd();
+}
